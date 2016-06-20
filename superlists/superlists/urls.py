edf6f8e76ab6d01ce1c7ce984934a5ad288d2d78
Marcin Turek <marcin.turek@poczta.fm>
2016-06-21 00:07:31 +0200

    Test updates

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -1,3 +1,10 @@
+from django.conf.urls import patterns, url, include
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    url(r'^$', 'lists.views.home_page', name='home'),
+)
+
 """superlists URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,9 +20,3 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
